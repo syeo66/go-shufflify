0c46713fe1ad65131d13af8257372b87fbfdca7a
Red Ochsenbein <red@redochsenbein.ch>
2024-07-15 15:50:22 +0200

    Add queue list autoresize

--- app/retrieveQueue.go
+++ app/retrieveQueue.go
@@ -24,9 +24,26 @@ func retrieveQueue(token string) (*Queue, error) {
 	body, _ := io.ReadAll(resp.Body)
 	err = json.Unmarshal(body, queue)
 
+	cleanQueue := &Queue{
+		CurrentlyPlaying: queue.CurrentlyPlaying,
+		Queue:            []Track{},
+	}
+
+	// for some reason the spotify api returns the same track multiple times
+	// even if it is not in the queue only once.
+	// TODO remove this later when Spotify works properly (which might be never)
+	var prevId string
+	for _, track := range queue.Queue {
+		if prevId == track.Id {
+			continue
+		}
+		cleanQueue.Queue = append(cleanQueue.Queue, track)
+		prevId = track.Id
+	}
+
 	if err != nil {
 		return nil, errors.Join(err, errors.New("error retrieving user"))
 	}
 
-	return queue, nil
+	return cleanQueue, nil
 }
---
+++ js/resize.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let style = document.createElement('style')
+
+  function doResize() {
+    const elm = document.querySelector('.queue')
+    if (elm) {
+      const top = elm.getBoundingClientRect().top
+      const viewPortHeight = window.innerHeight
+      const maxHeight = viewPortHeight - top - 16;
+      style.remove()
+      style = document.createElement('style')
+      style.innerHTML = `
+        .queue {
+          max-height: ${maxHeight}px;
+        }
+      `
+      document.head.appendChild(style)
+    }
+  }
+
+  window.addEventListener('resize', doResize)
+  doResize()
+})
