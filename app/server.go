cdc0c9e3d90e687057254ea3879227636670e03a
Red Ochsenbein <red@redochsenbein.ch>
2024-07-15 11:11:36 +0200

    add live data to player

--- app/server.go
+++ app/server.go
@@ -13,6 +13,11 @@ import (
 
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
+// TOOD
+// - display and update queue
+// - introduce queue manager (go routine)
+// - prevent queue manager and frontend from fetching the same data twice
+
 func main() {
 	port := getEnv("PORT", "3333")
 
@@ -22,16 +27,19 @@ func main() {
 	tmpl := make(map[string]*template.Template)
 	tmpl["index.html"] = template.Must(template.ParseFiles("templates/base.html", "templates/index.html", "templates/player.html"))
 	tmpl["login.html"] = template.Must(template.ParseFiles("templates/base.html", "templates/login.html"))
+	tmpl["player.html"] = template.Must(template.ParseFiles("templates/player.html"))
 
 	cssfs := http.FileServer(http.Dir("./css"))
 	http.Handle("/css/", http.StripPrefix("/css/", cssfs))
+
 	jsfs := http.FileServer(http.Dir("./js"))
 	http.Handle("/js/", http.StripPrefix("/js/", jsfs))
 
 	http.HandleFunc("/", getRoot(tmpl, db))
+	http.HandleFunc("/callback", getCallback(db))
 	http.HandleFunc("/login", getLogin(tmpl))
 	http.HandleFunc("/logout", getLogout)
-	http.HandleFunc("/callback", getCallback(db))
+	http.HandleFunc("/player", getPlayer(tmpl, db))
 
 	fmt.Printf("starting server port %s\n", port)
 	fmt.Printf("open http://localhost:%s/\n", port)
